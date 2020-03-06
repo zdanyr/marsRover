d318fb5e2a436500265f0b636adf16e532975571
Daniela Rodriguez <zdanyr@gmail.com>
2020-03-07 12:07:22 +1300

    added tests to check length and initial position

--- grid.js
+++ grid.js
@@ -1,9 +1,14 @@
-const createGrid = x => {
-  const array = new Array(x);
-  for (let i = 0; i < x; i++) {
-    array[i] = new Array(x);
+const createGrid = dimension => {
+  const array = new Array(dimension);
+  for (let i = 0; i < dimension; i++) {
+    array[i] = new Array(dimension);
   }
+  initializePositionRover(array);
   return array;
+};
+
+const initializePositionRover = array => {
+  array[(0, 0)] = 1;
 };
 
 module.exports = { createGrid };
--- grid.test.js
+++ grid.test.js
@@ -1,10 +1,27 @@
 const { createGrid } = require("./grid");
 
 describe("grid", () => {
-  test("should be 3x3 in dimension", () => {
+  test("Given a dimension when creating a grid then create a grid of the given dimension", () => {
     const result = createGrid(3);
-    expect(result[0].length).toBe(3);
+
+    console.log(`result cero dos ${result[(0, 2)]}`);
+    expect(result[(0, 2)]).toBeDefined();
+    expect(result[(0, 3)]).toBeUndefined();
     expect(result[1].length).toBe(3);
     expect(result[2].length).toBe(3);
   });
+
+  test("rover should initialize on point 0,0", () => {
+    const result = createGrid(3);
+    expect(result[(0, 0)]).toBe(1);
+  });
 });
+
+/*
+put rover on point 0,0
+move rover to 0,1
+put rover 0,2 then move rover to 0,0
+turn rover left right
+
+
+*/
