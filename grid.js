bb94958a19268bea30d421b09f9451fa1673bc7f
Daniela Rodriguez <zdanyr@gmail.com>
2020-03-09 09:01:29 +1300

    added tests and logic to initialize the rover

--- grid.js
+++ grid.js
@@ -1,14 +1,25 @@
 const createGrid = dimension => {
-  const array = new Array(dimension);
+  let array = new Array(dimension);
   for (let i = 0; i < dimension; i++) {
     array[i] = new Array(dimension);
   }
-  initializePositionRover(array);
+  array = initializeGrid(array);
+  array = initializePositionRover(array);
+  return array;
+};
+
+const initializeGrid = array => {
+  for (let x = 0; x < array.length; x++) {
+    for (let y = 0; y < array.length; y++) {
+      array[x][y] = 0;
+    }
+  }
   return array;
 };
 
 const initializePositionRover = array => {
-  array[(0, 0)] = 1;
+  array[0][0] = 1;
+  return array;
 };
 
 module.exports = { createGrid };
--- grid.test.js
+++ grid.test.js
@@ -3,22 +3,33 @@ const { createGrid } = require("./grid");
 describe("grid", () => {
   test("Given a dimension when creating a grid then create a grid of the given dimension", () => {
     const result = createGrid(3);
-
-    console.log(`result cero dos ${result[(0, 2)]}`);
     expect(result[(0, 2)]).toBeDefined();
     expect(result[(0, 3)]).toBeUndefined();
-    expect(result[1].length).toBe(3);
-    expect(result[2].length).toBe(3);
+    expect(result[(1, 2)]).toBeDefined();
+    expect(result[(1, 3)]).toBeUndefined();
+    expect(result[(2, 2)]).toBeDefined();
+    expect(result[(3, 3)]).toBeUndefined();
   });
+});
 
-  test("rover should initialize on point 0,0", () => {
+describe("position of rover", () => {
+  test("it should initialize on point 0,0", () => {
     const result = createGrid(3);
-    expect(result[(0, 0)]).toBe(1);
+    expect(result[0][0]).toBe(1);
+  });
+  test("when mars initialize, all positions in the grid, except 0,0 should not be 1", () => {
+    const grid = createGrid(3);
+    let result = false;
+    for (let row = 0; row < grid.length; row++) {
+      for (let column = 0; column < grid.length; column++) {
+        if (grid[row][column] === 1 && row != 0 && column != 0) result = true;
+      }
+    }
+    expect(result).toBe(false);
   });
 });
 
 /*
-put rover on point 0,0
 move rover to 0,1
 put rover 0,2 then move rover to 0,0
 turn rover left right
